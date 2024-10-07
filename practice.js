789e9d174a88534d3ab6ac898e8d70db440bd136
MN <mn@MNs-MacBook-Pro.local>
2024-10-07 17:23:16 +0500

    Initialize commit

---
+++ practice.js
@@ -0,0 +1,73 @@
+// var sum = () => 
+// {
+//     return 3 + 8;
+// }
+
+// console.log(sum() );
+// var arr = [3,6,8,9,10];
+// var sum = 0;
+// arr.forEach(function(i){
+// sum += i;}
+// )
+// console.log(sum);
+
+// function product(a,b=1)
+// {
+//     return (a * b);
+// }
+
+// console.log(product(2));
+
+// ------------------------
+
+// function factorial(a)
+// {
+//     var result = 1;
+
+//     for (i = 1; i<=a; i++)
+//     {
+//         result *= i;
+//     }
+
+//     return result;
+
+// }
+
+// console.log((factorial(5)));
+
+//-----------------------------
+
+//  function reverseString(a)
+//  {
+//     var len = a.length;
+
+//     result = "";
+
+//     for(i = len; i>=0; i--)
+//     {
+//         result += a.charAt(i);
+//     }
+
+//     return result;
+//  }
+
+//  console.log(reverseString("talha")); 
+
+//------------------------------------
+
+// Closure Concept
+
+// function outer(num)
+// {
+//     function inner()
+//     {
+//         return num + 5;
+//     }
+
+//     return inner;
+// }
+
+// let result = outer(5);
+// console.log(result());
+
+ 
